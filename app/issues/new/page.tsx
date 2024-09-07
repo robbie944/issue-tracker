020d7658cbe87e55b41aa4a61f0b8a0cc9d2562a
Robin Chandler <robin.chandler944@gmail.com>
2024-09-07 04:47:46 -0600

    Add a narkdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,13 +1,14 @@
 'use client';
 import { Button, TextField } from '@radix-ui/themes'
-import React from 'react'
+import SimpleMDE from "react-simplemde-editor";
+import "easymde/dist/easymde.min.css";
 import { TextArea } from '@radix-ui/themes'
 
 const NewIssuePage = () => {
   return (
     <div className='max-w-xl space-y-3'>
         <TextField.Root size ="2" placeholder = "Title"/>
-        <TextArea placeholder="Description" />
+        <SimpleMDE placeholder="Description" />
         <Button>Submit New Issue</Button>
 
     
